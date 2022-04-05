7f177cecd157d5ed435acb90b1a2efb6126326dd
Phil Sautter <20444474+redeux@users.noreply.github.com>
2022-04-05 11:44:19 -0400

    Cleanup code and add README content

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # terraform-run-task-hello-world-nodejs
 A Terraform Cloud Run Task Hello World in Node.js
+
+This is a basic implementation of a Terraform Cloud Run Task in Node.js. Run easily with [NGROK](https://ngrok.com/) or in [Heroku](https://www.heroku.com/).
+
+## Getting Started Locally
+Make sure you have Node.js v16.x and NPM v8.6. You can find the Node.js and NPM installation instructions [here](https://docs.npmjs.com/downloading-and-installing-node-js-and-npm).
+
+```
+node -v
+npm -v
+```
+
+Install the Run Task server dependencies
+```
+npm install
+```
+
+Start the Run Task server
+```
+npm start
+```
+NOTE: By default, the service will start using port `3000` and using an HMAC key of `abc123`. These can be set using the environment variables `PORT` and `HMAC_KEY` respectively.
+
+Start NGROK using the port you specified
+```bash
+ngrok http $PORT
+```
--- index.js
+++ index.js
@@ -5,29 +5,33 @@ const {
 } = require('crypto')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 // Configure Express middleware to parse the JSON body and validate the HMAC
 app.use(express.json(), validateHmac)
 
 app.post('/', (req, res) => {
     // Send a 200 to tell Terraform Cloud that we recevied the Run Task
+    // Documentation - https://www.terraform.io/cloud-docs/api-docs/run-tasks-integration#run-task-request
     res.sendStatus(200)
 
     // Do some processing on the Run Task request
-    console.log(`Run Task payload = ${JSON.stringify(req.body, null, 2)}`)
+    // Schema Documentation - https://www.terraform.io/cloud-docs/api-docs/run-tasks-integration#request-body
+
+    // When a user adds a new Run Task to their Terraform Cloud organization, Terraform Cloud will attempt to 
+    // validate the Run Task address by sending a payload with dummy data. This condition will have to be accounted for.
+    console.log(`Run Task payload\n${JSON.stringify(req.body, null, 2)}`)
 
     // Send the results back to Terraform Cloud
-    return sendCallback(req.body.task_result_callback_url, req.body.access_token, 'passed', "Hello World")
+    return sendCallback(req.body.task_result_callback_url, req.body.access_token, 'passed', "Hello World", "http://example.com/runtask/QxZyl")
 })
 
 function validateHmac(req, res, next) {
-    const hmacKey = 'abc123'
-    const hmac = createHmac('sha512', hmacKey)
-    hmac.update(JSON.stringify(req.body))
-    const computedHmac = hmac.digest('hex')
+    const hmacKey = process.env.HMAC_KEY || 'abc123'
+    const computedHmac = createHmac('sha512', hmacKey).update(JSON.stringify(req.body)).digest('hex')
     const remoteHmac = req.get('x-tfc-task-signature')
     // If the HMAC validation fails, log the error and send an HTTP Status Code 401, Unauthorized
+    // Currently undocumented but 401 is the expected response for an invalid HMAC
     if (computedHmac !== remoteHmac) {
         console.log(`HMAC validation failed. 
         Expected ${remoteHmac} 
@@ -37,15 +41,16 @@ function validateHmac(req, res, next) {
     next()
 }
 
-function sendCallback(callbackUrl, accessToken, status, msg) {
+function sendCallback(callbackUrl, accessToken, status, message, url) {
     // Format the payload for the callback
+    // Schema Documentation - https://www.terraform.io/cloud-docs/api-docs/run-tasks-integration#request-body-1
     const data = JSON.stringify({
         "data": {
             "type": "task-results",
             "attributes": {
-                "status": status,
-                "message": msg,
-                "url": "http://my.supercool.com/runtask"
+                status,
+                message,
+                url,
             }
         }
     })
@@ -57,16 +62,16 @@ function sendCallback(callbackUrl, accessToken, status, msg) {
         port: 443,
         path: callbackUrl.pathname,
         method: 'PATCH',
+        // Documentation - https://www.terraform.io/cloud-docs/api-docs/run-tasks-integration#request-headers-1
         headers: {
             'Content-Type': 'application/vnd.api+json',
-            'Content-Length': data.length,
             'Authorization': 'Bearer ' + accessToken
         }
     }
 
     // Send the callback
     const req = https.request(options, res => {
-        console.log(`statusCode: ${res.statusCode}`)
+        console.log(`Callback statusCode: ${res.statusCode}`)
 
         res.on('data', d => {
             process.stdout.write(d)
